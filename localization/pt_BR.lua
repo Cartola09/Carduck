646cca4604cd6383d7fee8f3a16c24490b9b7a82
Cartola09 <118937368+Cartola09@users.noreply.github.com>
2026-03-01 18:24:32 -0300

    fix localization

--- localization/pt_BR.lua
+++ localization/pt_BR.lua
@@ -275,7 +275,7 @@ return {
             dictionary = {
                 k_cd_zero_reversed = "ZERO_REVERSO: SEM_ENERGIA_SUFICIENTE",
                 k_cd_arcana_active = "O Arcano se revela...",
-                k_cd_uzumaki = "再度",
+                k_cd_uzumaki = "Denovo.",
 
             },
         },
