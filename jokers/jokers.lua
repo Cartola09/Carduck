e2881db8e39eea0adf0b379f5f678f63253449e0
Cartola09 <118937368+Cartola09@users.noreply.github.com>
2026-03-02 17:13:49 -0300

    fix jevil

--- jokers/jokers.lua
+++ jokers/jokers.lua
@@ -1438,7 +1438,7 @@ calculate = function(self, card, context)
                         j_triggered = true
                     end
 
-                    if j.ability and j.ability.x_mult and j.ability.x_mult > 1 then
+                    if j.ability and j.ability.x_mult and j.ability.x_mult ~= 0 then
                         j.ability.x_mult = (pseudorandom('jevil_x') * 6)
                         j_triggered = true
                     end
